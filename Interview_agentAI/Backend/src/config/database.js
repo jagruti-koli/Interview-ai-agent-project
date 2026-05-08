2d7562593e2d97392911f431dfe5669e52b72555
jagruti koli <jagrutikoli6201@gmail.com>
2026-05-08 13:50:53 +0530

    Removed MongoDB URI log

--- Interview_agentAI/Backend/src/config/database.js
+++ Interview_agentAI/Backend/src/config/database.js
@@ -1,15 +1,17 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 async function connectToDB() {
     try {
-        console.log("MONGO_URI:", process.env.MONGO_URI) 
 
-        await mongoose.connect(process.env.MONGO_URI)
+        await mongoose.connect(process.env.MONGO_URI);
 
         console.log("Connected to Database");
+
     } catch (err) {
+
         console.log("Connect to Database failed!", err.message);
+
     }
 }
 
-module.exports = connectToDB
+module.exports = connectToDB;
