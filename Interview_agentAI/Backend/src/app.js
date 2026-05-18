aee09f7f387b3c6436fc2d83784db1847a6688dd
jagruti koli <jagrutikoli6201@gmail.com>
2026-05-18 13:58:16 +0530

    fixed changes

--- Interview_agentAI/Backend/src/app.js
+++ Interview_agentAI/Backend/src/app.js
@@ -18,7 +18,6 @@ const chatRouter = require("./routes/chat.routes")
 
 app.use('/api/auth', authRouter)
 app.use('/api/interview', interviewRouter)
-app.use('/api/auth', authRouter)
 app.use("/api/chat", chatRouter)
 
 module.exports = app
